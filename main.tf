37c727185861be4d45bbaef260d50194f7269f12
Grigoriy Azatyan <83419248+GrigoriyAzatyan@users.noreply.github.com>
2021-10-19 00:15:46 +0500

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,27 @@
+provider "aws" {
+   region = "eu-west-2"
+}
+
+
+data "aws_ami" "latest-ubuntu" {
+most_recent = true
+owners = ["099720109477"] # Canonical
+
+  filter {
+      name   = "name"
+      values = ["ubuntu*"]
+  }
+
+  filter {
+      name   = "virtualization-type"
+      values = ["hvm"]
+  }
+}
+
+
+data "aws_caller_identity" "current" {}
+
+
+data "aws_region" "current" {}
+
+
